c6c48dbcb5d3979c480a0dc426f5dfc1fcce0e6f
anjali0131 <2006anjalikumari1111@gmail.com>
2026-06-01 21:59:42 +0530

    Add remaining Day 1 solutions

---
+++ Week_1/Day_1/remove_duplicates.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 1;
+        int n = nums.size();  
+        for(int i = 1; i < n; i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[k++] = nums[i];  
+            }
+        }
+        return k;
+    }
+};

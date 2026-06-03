065d043ceba04a1445becfff9989dc891422df2e
anjali0131 <2006anjalikumari1111@gmail.com>
2026-06-04 01:27:26 +0530

    day 3 1 and 2

---
+++ Week_1/Day_3/move_zeroes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+
+        for(int right = 0; right<n; right++){
+            if(nums[right]!= 0){
+                int temp =  nums[right];
+                nums[right]= nums[left];
+                nums[left]= temp;
+                left++;
+            }
+        }
+        
+    }
+};
